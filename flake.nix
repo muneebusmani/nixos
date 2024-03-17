96bd39242bacaf1094f8995f48b3d09f65ecf84c
Your Name <you@example.com>
2024-03-18 01:49:42 +0500

    merged home manager and nixos and converted to flakes

--- flake.nix
+++ flake.nix
@@ -8,10 +8,10 @@
       system = "x86_64-linux"; # replace this with your actual system type
       wslDistroName = builtins.getEnv "WSL_DISTRO_NAME";
       isNixOS = builtins.getEnv "NIX_OS" == "nixos";
-      config = if system == "x86_64-darwin" then ./darwin.nix
-               else if system == "x86_64-linux" && wslDistroName == "" then ./nixos.nix
-               else if system == "x86_64-linux" && wslDistroName != "" && isNixOS then ./nixos-wsl.nix
-               else if system == "x86_64-linux" && wslDistroName != "" && !isNixOS then ./wsl.nix
+      config = if system == "x86_64-darwin" then ./darwin/configuration.nix
+               else if system == "x86_64-linux" && wslDistroName == "" then ./nixos/configuration.nix
+               else if system == "x86_64-linux" && wslDistroName != "" && isNixOS then ./nixos-wsl/configuration.nix
+               else if system == "x86_64-linux" && wslDistroName != "" && !isNixOS then ./wsl/configuration.nix
                else ./configuration.nix;
     in {
       nixos = nixpkgs.lib.nixosSystem {
