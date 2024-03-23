aca52993e86e3bf563842b57a9522cdd2b22a833
Muneeb Usmani <muneebusmani8355@gmail.com>
2024-03-23 20:57:13 +0500

    added more stuff

--- flake.nix
+++ flake.nix
@@ -1,23 +1,24 @@
 {
   description = "My NixOS configuration";
-
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
   outputs = { self, nixpkgs }: {
-    nixosConfigurations = let
-      system = "x86_64-linux"; # replace this with your actual system type
-      wslDistroName = builtins.getEnv "WSL_DISTRO_NAME";
-      isNixOS = builtins.getEnv "NIX_OS" == "nixos";
-      config = if system == "x86_64-darwin" then ./darwin/configuration.nix
-               else if system == "x86_64-linux" && wslDistroName == "" then ./nixos/configuration.nix
-               else if system == "x86_64-linux" && wslDistroName != "" && isNixOS then ./nixos-wsl/configuration.nix
-               else if system == "x86_64-linux" && wslDistroName != "" && !isNixOS then ./wsl/configuration.nix
-               else ./configuration.nix;
-    in {
-      nixos = nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [ config ];
+    nixosConfigurations =
+      let
+        system = "x86_64-linux"; # replace this with your actual system type
+        #wslDistroName = builtins.getEnv "WSL_DISTRO_NAME";
+        #isNixOS = builtins.getEnv "NIX_OS" == "nixos";
+        #config = if system == "x86_64-darwin" then ./darwin/configuration.nix
+        #         else if system == "x86_64-linux" && wslDistroName == "" then ./nixos/configuration.nix
+        #         else if system == "x86_64-linux" && wslDistroName != "" && isNixOS then ./nixos-wsl/configuration.nix
+        #         else if system == "x86_64-linux" && wslDistroName != "" && !isNixOS then ./wsl/configuration.nix
+        #         else ./configuration.nix;
+        config = ./nixos/configuration.nix;
+      in
+      {
+        nixos = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [ config ];
+        };
       };
-    };
   };
 }
