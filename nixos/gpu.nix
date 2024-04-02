1a84002a3a0d378bc933b86a1772afd2b40daaaf
Muneeb Usmani <muneebusmani8355@gmail.com>
2024-04-02 17:51:54 +0500

    added more stuff

--- nixos/gpu.nix
+++ nixos/gpu.nix
@@ -1,7 +1,5 @@
 { config, lib, pkgs, ... }:
 {
-  specialisation = {
-    nvidia.configuration = {
       system.nixos.tags = [ "nvidia" ];
       hardware.opengl = {
         enable = true;
@@ -11,7 +9,7 @@
       powerManagement.cpuFreqGovernor = lib.mkForce "performance";
       hardware.nvidia = {
         modesetting.enable = true;
-        powerManagement.enable = true;
+        powerManagement.enable = false;
         powerManagement.finegrained = false;
         open = false;
         nvidiaSettings = true;
@@ -22,17 +20,4 @@
           nvidiaBusId = "PCI:1:0:0";
         };
       };
-    };
-    powersave.configuration = {
-      system.nixos.tags = [ "powersave" ];
-      services.preload.enable = lib.mkForce false;
-      hardware.nvidiaOptimus.disable = lib.mkForce true;
-      hardware.bluetooth.enable = lib.mkForce false; # enables support for Bluetooth
-      hardware.bluetooth.powerOnBoot = lib.mkForce false; # powers up the default Bluetooth controller on boot
-      virtualisation.docker.enable = lib.mkForce false;
-      users.users.muneeb.extraGroups = lib.mkForce [ "networkmanager" "audio" "wheel" ];
-      powerManagement.cpuFreqGovernor = lib.mkForce "schedutil";
-      zramSwap.enable = lib.mkForce false;
-    };
-  };
 }
